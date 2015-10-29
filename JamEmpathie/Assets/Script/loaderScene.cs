6bb02d0717a83c9dc9bbaf389907588ec13c5794
Treveur <ramauge.benjamin@gmail.com>
2015-10-29 13:39:48 +0100

    [modif]preloader done but to ameliorate

--- JamEmpathie/Assets/Script/loaderScene.cs
+++ JamEmpathie/Assets/Script/loaderScene.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class loaderScene : MonoBehaviour {
 
     private AsyncOperation async;
+
+    [SerializeField]
+    private Text _progress;
 
     // Use this for initialization
     void Start () {
@@ -12,6 +16,7 @@ public class loaderScene : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        _progress.text = (async.progress * 100) + "%";
         Debug.Log(async.progress);
 	}
 }
