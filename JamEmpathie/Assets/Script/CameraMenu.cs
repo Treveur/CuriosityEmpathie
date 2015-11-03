9b87c76fe94d0d55509d064def6eed0b529f7426
Treveur <ramauge.benjamin@gmail.com>
2015-11-03 11:47:44 +0100

    [Modif] Camera turn around rover but to ameliorate

--- JamEmpathie/Assets/Script/CameraMenu.cs
+++ JamEmpathie/Assets/Script/CameraMenu.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class CameraMenu : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject focusObject;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    private Vector3 point;
+
 	// Use this for initialization
 	void Start () {
-	
+        point = focusObject.transform.position;
+        transform.LookAt(point);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.RotateAround(point, new Vector3(0f, 1f, 0f), 20 * Time.deltaTime * rotationSpeed);
 	}
 }
